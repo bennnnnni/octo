a2e99ab0597dea7131b920fb51b3a8b532b26725
Benjamin Binder <benjamin.binder@casavi.de>
2023-11-04 12:35:56 +0100

    use CSR for product page

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,3 +1,26 @@
+import { useQuery } from "@apollo/client";
+import { PRODUCT_BY_ID } from "../operations/product";
+
+const PRODUCT_ID = "1";
+
 export default function Product() {
-  return <div>Product page</div>;
+  const { data, loading, error } = useQuery(PRODUCT_BY_ID, {
+    variables: { id: PRODUCT_ID },
+  });
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  const product = data.Product;
+  return (
+    <div>
+      <div>{`Client side rendering`}</div>
+      <div>{product.id}</div>
+    </div>
+  );
 }
