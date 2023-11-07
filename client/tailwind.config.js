58b879b1472f429245bdb596cbc7fb23ae87100a
Benjamin Binder <benjamin.binder@casavi.de>
2023-11-07 20:23:29 +0100

    add tailwind configs

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    colors: {
+      hemocyanin: "#180048",
+      ice: "#f0ffff",
+      plum: "#600e6b",
+      purpleHaze: "#a49fc8",
+      siphon: "#100030",
+      sohoLights: "#f050f8",
+    },
+  },
+  plugins: [],
+};
